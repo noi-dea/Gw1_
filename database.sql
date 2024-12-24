754a114dcda3bc1b20aa7c6f04527503b8f91b62
Dean Noiron <noirondean@gmail.com>
2024-12-24 14:04:27 +0100

    tabel autos#1

---
+++ database.sql
@@ -0,0 +1,38 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`cars`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`cars` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `brand` VARCHAR(45) NOT NULL,
+  `model` VARCHAR(45) NOT NULL,
+  `year` YEAR NOT NULL,
+  `fueltype` ENUM("benzine", "diesel", "hybride", "elektrisch") NOT NULL,
+  `colour` VARCHAR(45) NOT NULL,
+  `doors` TINYINT NOT NULL,
+  `transmission` ENUM("handmatig", "automatisch") NOT NULL,
+  `price` DECIMAL(10,2) NOT NULL,
+  `mileage` INT NOT NULL,
+  `bodywork` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
