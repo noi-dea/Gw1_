66d92e8a2e75ba304ed9380c018de015b5885c93
Dean Noiron <noirondean@gmail.com>
2024-12-26 15:06:56 +0100

    added appointments table and otm connection between users and cars#7

--- database.sql
+++ database.sql
@@ -5,59 +5,35 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema gw1
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema gw1
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `gw1` DEFAULT CHARACTER SET utf8mb3 ;
+USE `gw1` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`fotosets`
+-- Table `gw1`.`fotosets`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`fotosets` (
+CREATE TABLE IF NOT EXISTS `gw1`.`fotosets` (
   `id` INT NOT NULL,
-  `front` VARCHAR(45) NULL,
-  `back` VARCHAR(45) NULL,
-  `inner` VARCHAR(45) NULL,
+  `front` VARCHAR(150) NULL DEFAULT NULL,
+  `back` VARCHAR(150) NULL DEFAULT NULL,
+  `inner` VARCHAR(150) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`cars`
+-- Table `gw1`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`cars` (
+CREATE TABLE IF NOT EXISTS `gw1`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `brand` VARCHAR(45) NOT NULL,
-  `model` VARCHAR(45) NOT NULL,
-  `year` YEAR NOT NULL,
-  `fueltype` ENUM("benzine", "diesel", "hybride", "elektrisch") NOT NULL,
-  `colour` VARCHAR(45) NOT NULL,
-  `doors` TINYINT NOT NULL,
-  `transmission` ENUM("handmatig", "automatisch") NOT NULL,
-  `price` DECIMAL(10,2) NOT NULL,
-  `mileage` INT NOT NULL,
-  `bodywork` VARCHAR(45) NOT NULL,
-  `fotoUrl` VARCHAR(45) NOT NULL,
-  `fotosets_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `fotosets_id`),
-  INDEX `fk_cars_fotosets1_idx` (`fotosets_id` ASC) VISIBLE,
-  CONSTRAINT `fk_cars_fotosets1`
-    FOREIGN KEY (`fotosets_id`)
-    REFERENCES `mydb`.`fotosets` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
   `firstname` VARCHAR(45) NOT NULL,
   `lastname` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
@@ -65,16 +41,52 @@ CREATE TABLE IF NOT EXISTS `mydb`.`users` (
   `street` VARCHAR(45) NOT NULL,
   `postalcode` INT NOT NULL,
   `housenumber` SMALLINT NOT NULL,
-  `isAdmin` TINYINT NULL DEFAULT 0,
+  `bus` VARCHAR(1) NULL DEFAULT NULL,
+  `isAdmin` TINYINT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
-ENGINE = InnoDB;
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`wishlist`
+-- Table `gw1`.`cars`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`wishlist` (
+CREATE TABLE IF NOT EXISTS `gw1`.`cars` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `brand` VARCHAR(45) NOT NULL,
+  `model` VARCHAR(45) NOT NULL,
+  `year` YEAR NOT NULL,
+  `fueltype` ENUM('benzine', 'diesel', 'hybride', 'elektrisch') NOT NULL,
+  `colour` VARCHAR(45) NOT NULL,
+  `doors` TINYINT NOT NULL,
+  `transmission` ENUM('handmatig', 'automatisch') NOT NULL,
+  `price` DECIMAL(10,2) NOT NULL,
+  `mileage` INT NOT NULL,
+  `bodywork` VARCHAR(45) NOT NULL,
+  `fotoUrl` VARCHAR(150) NOT NULL,
+  `fotosets_id` INT NOT NULL,
+  `users_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `fotosets_id`, `users_id`),
+  INDEX `fk_cars_fotosets1_idx` (`fotosets_id` ASC) VISIBLE,
+  INDEX `fk_cars_users1_idx` (`users_id` ASC) VISIBLE,
+  CONSTRAINT `fk_cars_fotosets1`
+    FOREIGN KEY (`fotosets_id`)
+    REFERENCES `gw1`.`fotosets` (`id`),
+  CONSTRAINT `fk_cars_users1`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `gw1`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `gw1`.`wishlist`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gw1`.`wishlist` (
   `cars_id` INT NOT NULL,
   `users_id` INT NOT NULL,
   PRIMARY KEY (`cars_id`, `users_id`),
@@ -82,15 +94,58 @@ CREATE TABLE IF NOT EXISTS `mydb`.`wishlist` (
   INDEX `fk_cars_has_users_cars_idx` (`cars_id` ASC) VISIBLE,
   CONSTRAINT `fk_cars_has_users_cars`
     FOREIGN KEY (`cars_id`)
-    REFERENCES `mydb`.`cars` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES `gw1`.`cars` (`id`),
   CONSTRAINT `fk_cars_has_users_users1`
     FOREIGN KEY (`users_id`)
-    REFERENCES `mydb`.`users` (`id`)
+    REFERENCES `gw1`.`users` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `gw1`.`appointments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gw1`.`appointments` (
+  `id` INT NOT NULL,
+  `date` DATETIME NOT NULL,
+  `location` VARCHAR(100) NOT NULL,
+  `cars_id` INT NOT NULL,
+  `cars_fotosets_id` INT NOT NULL,
+  `cars_users_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `cars_id`, `cars_fotosets_id`, `cars_users_id`),
+  INDEX `fk_appointments_cars1_idx` (`cars_id` ASC, `cars_fotosets_id` ASC, `cars_users_id` ASC) VISIBLE,
+  CONSTRAINT `fk_appointments_cars1`
+    FOREIGN KEY (`cars_id` , `cars_fotosets_id` , `cars_users_id`)
+    REFERENCES `gw1`.`cars` (`id` , `fotosets_id` , `users_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `gw1`.`users_has_appointments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gw1`.`users_has_appointments` (
+  `users_id` INT NOT NULL,
+  `appointments_id` INT NOT NULL,
+  `appointments_cars_id` INT NOT NULL,
+  `appointments_cars_fotosets_id` INT NOT NULL,
+  `appointments_cars_users_id` INT NOT NULL,
+  PRIMARY KEY (`users_id`, `appointments_id`, `appointments_cars_id`, `appointments_cars_fotosets_id`, `appointments_cars_users_id`),
+  INDEX `fk_users_has_appointments_appointments1_idx` (`appointments_id` ASC, `appointments_cars_id` ASC, `appointments_cars_fotosets_id` ASC, `appointments_cars_users_id` ASC) VISIBLE,
+  INDEX `fk_users_has_appointments_users1_idx` (`users_id` ASC) VISIBLE,
+  CONSTRAINT `fk_users_has_appointments_users1`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `gw1`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_has_appointments_appointments1`
+    FOREIGN KEY (`appointments_id` , `appointments_cars_id` , `appointments_cars_fotosets_id` , `appointments_cars_users_id`)
+    REFERENCES `gw1`.`appointments` (`id` , `cars_id` , `cars_fotosets_id` , `cars_users_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
