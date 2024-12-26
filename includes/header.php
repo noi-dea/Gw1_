99a9286cd9d39e5e3152a2178f8eb0e460a7d0b9
Serap Cakmak <serap.cakmak@hotmail.com>
2024-12-26 23:03:49 +0100

    index.php bijwerken

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<header>
+    <nav>
+        <h1>Groeps Weirk</h1>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="contact">Contact</a></li>
+            <li><a href="wishlist">Mijn Wishlist</a></li>
+        </ul>
+        <ul>
+            <li><a href="login">Login</a></li>
+            <li><a href="register">Registreren</a></li>
+            <li><a href="admin">Admin</a></li>
+            <li><a href="logout">Logout</a></li>
+        </ul>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include_once "includes/css_js.inc.php";
 ?>
 <!DOCTYPE html>
@@ -8,15 +12,20 @@ include_once "includes/css_js.inc.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WEBSITE HOMEPAGE</title>
+    <title>GROEPS WEIRK</title>
     <link rel="stylesheet" href="./dist/<?= $cssPath ?>" />
     <script type="module" src="./dist/<?= $jsPath ?>"></script>
 </head>
 
 <body>
-    <?= "php works on front website" ?>
-    <p class="icon-pacman"></p>
-    <img src="images/sample.jpg" alt="">
+    <?php include 'includes/header.php'; ?>
+    <main>
+        <?php include 'includes/banner.php'; ?>
+        <?php include 'includes/searchfilter.php'; ?>
+        <?php include 'includes/categorie.php'; ?>
+
+    </main>
+    <?php include 'includes/footer.php'; ?>
 </body>
 
 </html>
