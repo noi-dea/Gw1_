09ad7dc1344ccf0c1257f12f61e1c49e7b055b80
Sjabloons <kristoflafaut@gmail.com>
2024-12-28 10:23:16 +0100

    login

--- login.php
+++ login.php
@@ -1,5 +1,21 @@
 <?php
 
+require("functions.inc.php");
+
+$errors = [];
+
+if (isset($_POST["button"])) {
+    if (!strlen($_POST["inputusername"])) {
+        $errors[] = "Please fill in username";
+    }
+    if (!strlen($_POST["inputpassword"])) {
+        $errors[] = "Please fill in password";
+    }
+    $uid = isValidLogin($_POST["inputusername"], $_POST["inputpassword"]);
+    if ($uid) {
+        header("Location: ");
+    }
+}
 
 ?>
 
