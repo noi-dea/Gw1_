43c24cab4627400fab0949b521734672f424dd92
Serap Cakmak <serap.cakmak@hotmail.com>
2024-12-29 13:12:39 +0100

    GW_009_require ../functions.inc.php

--- includes/hp_searchfilter.php
+++ includes/hp_searchfilter.php
@@ -3,8 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require './functions.inc.php';
-require 'functions.inc.php';
+require '../functions.inc.php';
 $makes = getMakes();
 $colours = getColours();
 $bodies = getBodyworks();
