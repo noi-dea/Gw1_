5ea69a7ef5c5f44040ba4a3bb0bbf4d25ffa48e4
Dean Noiron <noirondean@gmail.com>
2025-01-13 21:50:25 +0100

    added ogtags to carlistpage

---
+++ includes/ogTags.php
@@ -0,0 +1,20 @@
+<!-- Set variables before inclusion: $ogPath, $ogImg, $ogDesc -->
+<!-- HTML Meta Tags -->
+<meta name="description" content="<?= $ogDesc ?>">
+
+<!-- Facebook Meta Tags -->
+<meta property="og:url" content="https://www.groepsweirk.com/<?= $ogPath; ?>">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Groepsweirk">
+<meta property="og:description" content="<?= $ogDesc ?>">
+<meta property="og:image" content="<?= $ogImg; ?>">
+
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="<?= $ogImg; ?>">
+<meta property="twitter:domain" content="groepsweirk.com">
+<meta property="twitter:url" content="https://www.groepsweirk.com/<?= $ogPath; ?>">
+<meta name="twitter:title" content="Groepsweirk">
+<meta name="twitter:description" content="<?= $ogDesc ?>">
+<meta name="twitter:image" content="<?= $ogImg; ?>">
+
+<!-- Meta Tags Generated via https://www.opengraph.xyz -->
